f57d01f6862a84f54dc881cae7a6ab05a89ac5a6
wyatt <35381759+fwd-sol-group@users.noreply.github.com>
2023-11-10 12:09:03 -0800

    finishing project: add task, forms, submiut and create, toggle ui, router

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
-import {Task} from '../../Task'
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   tasks: Task[] = [];
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
   deleteTask(task: Task) {
@@ -21,11 +21,15 @@ export class TasksComponent {
       .deleteTask(task)
       .subscribe(
         () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
-        );
+      );
   }
 
   toggleReminder(task: Task) {
     task.reminder = !task.reminder;
-    this.taskService.updateTaskReminder(task).subscribe()
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
   }
 }
